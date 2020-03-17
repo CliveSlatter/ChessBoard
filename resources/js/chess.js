d4d25088947fc3e7a71c70039e1f17f2a27d3d7a
cslatter@farnborough.ac.uk <PR.56Ome69th.40EUS>
2020-03-17 16:03:42 +0000

    Default Changelist

--- resources/js/chess.js
+++ resources/js/chess.js
@@ -1,38 +1,44 @@
 function runThis(){
     Draw();
-    FogOfWar();
+    //FogOfWar();
 }
 
+// size of each chess square
+const squareSize = 15;
+// position of board's top left
+const boardTopx = 50;
+const boardTopy = 50;
+
+let maxRooms = 25;
+
+let x = Math.floor((window.innerWidth-100) / squareSize);
+let y = Math.floor((window.innerHeight-100) / squareSize);
+let array = new Array(y);
+
 function Draw(){
-        // size of each chess square
-        const squareSize = 25;
-        // position of board's top left
-        const boardTopx = 50;
-        const boardTopy = 50;
-    alert(window.innerHeight + " " + window.innerWidth);
+
     let canvas = document.getElementById("canvasChessboard");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     context = canvas.getContext("2d");
 
-    let x = Math.floor((window.innerWidth-100) / squareSize);
-    let y = Math.floor((window.innerHeight-100) / squareSize);
-    let array = new Array(y);
-
-    alert(x + " " +y);
-
     for(let i = 0; i < y; i++){
         array[i] = new Array(x);
         for(let j = 0; j < x; j++){
-            array[i][j] = Math.floor(Math.random()*2);
+            array[i][j] = 0;
+            //array[i][j] = Math.floor(Math.random()*2);
         }
     }
+
+    addRooms();
+
         for(let i=0; i<y; i++) {
             for(let j=0; j<x; j++) {
-                context.fillStyle = ((array[i][j])%2===0) ? "purple":"orange";
+                context.fillStyle = ((array[i][j])%2===0) ? "black":"white";
                 let xOffset = boardTopx + j*squareSize;
                 let yOffset = boardTopy + i*squareSize;
-                context.fillRect(xOffset, yOffset, squareSize, squareSize);
+                context.fillRect(xOffset, yOffset, squareSize-1, squareSize-1);
+                context.strokeRect(xOffset,yOffset,squareSize,squareSize);
             }
         }
         // draw the border around the chessboard
@@ -63,3 +69,24 @@ function FogOfWar(){
         }
     }
 }
+
+function addRooms(){
+    let roomXMax = 10;
+    let roomYMax = 10;
+    let roomXYMin = 2;
+    let a = 0;
+    let b = 0;
+    let roomX = Math.floor(Math.random()*roomXMax);
+    let roomY = Math.floor(Math.random()*roomYMax);
+    do {
+        a = Math.floor(Math.random() * x);
+        b = Math.floor(Math.random() * y);
+    } while(a + roomX > x && b + roomY > y);
+    for(let i = a; i < a+roomX;i++){
+        for(let j = b; j < b + roomY; j++){
+            array[i][j] = 1;
+        }
+    }
+
+}
+
